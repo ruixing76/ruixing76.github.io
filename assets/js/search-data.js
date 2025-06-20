f7b49642ed625e0a5a1d13e13bc82f41e79a8638
ruixing76 <im.xingrui@gmail.com>
2025-06-20 11:53:31 +0000

    Deploying to gh-pages from @ ruixing76/ruixing76.github.io@d112f443d4cf814efb6b97ddb2bc6dd08e11ad3b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -469,11 +469,18 @@ ninja.data = [{
           window.open("mailto:%69%6D.%78%69%6E%67%72%75%69@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/ruixing76", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/xingrui", "_blank");
         },
       },{
         id: 'social-rss',
@@ -488,13 +495,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=BozhDOcAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
